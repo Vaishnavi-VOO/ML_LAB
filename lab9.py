9f98d6a1525fdf446c11880e4c7a800c159a0f0b
Vaishnavi-VOO <163522889+Vaishnavi-VOO@users.noreply.github.com>
2026-06-24 22:19:47 +0530

    Refactor code for loading and training on dataset

--- lab9.py
+++ lab9.py
@@ -5,17 +5,13 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 import matplotlib.pyplot as plt
 
-# Load Olivetti Faces dataset
 data = fetch_olivetti_faces(shuffle=True, random_state=42)
 X = data.data
 y = data.target
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42, stratify=y)
-
 gnb = GaussianNB()
-
 gnb.fit(X_train, y_train)
-
 y_pred = gnb.predict(X_test)
 
 accuracy = accuracy_score(y_test, y_pred)
